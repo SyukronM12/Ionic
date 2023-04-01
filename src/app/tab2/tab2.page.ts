88058a347900d0739522e91ad93b93ac7a3bd67a
unknown <msyukronm12@gmail.com>
2023-04-01 13:56:09 +0700

    update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { MATCH } from '../../assets/data';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -9,11 +10,11 @@ import { MATCH } from '../../assets/data';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, ExploreContainerComponent, CommonModule]
 })
 export class Tab2Page {
 
-  // constructor() {}
+  constructor() {}
   public match = MATCH;
 
 }
